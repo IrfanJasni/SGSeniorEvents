2f517b8930e928e68bfa323d1679a2cb4edee317
MUHAMMAD IRFAN BIN JASNI 2105205E <2105205E@student.tp.edu.sg>
2024-02-04 23:10:17 +0800

    Added Blazor List Pages with Delete Function

---
+++ SGSeniorEvents/Client/Static/Endpoints.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SGSeniorEvents.Client.Static
+{
+    public static class Endpoints
+    {
+        public static readonly string MakesEndpoint = "api/makes";
+        public static readonly string ModelsEndpoint = "api/models";
+        public static readonly string ColoursEndpoint = "api/colours";
+        public static readonly string VehiclesEndpoint = "api/vehicles";
+        public static readonly string CustomersEndpoint = "api/customers";
+        public static readonly string BookingsEndpoint = "api/bookings";
+    }
+}
--- SGSeniorEvents/Server/Controllers/BookingsController.cs
+++ SGSeniorEvents/Server/Controllers/BookingsController.cs
@@ -27,7 +27,8 @@ namespace SGSeniorEvents.Server.Controllers
         public async Task<IActionResult> GetBookings()
         {
 
-            var Bookings = await _unitOfWork.Bookings.GetAll();
+            var Bookings = await _unitOfWork.Bookings.GetAll(includes: q => q.Include(x =>
+            x.Vehicle).Include(x => x.Customer));
             return Ok(Bookings);
         }
 
